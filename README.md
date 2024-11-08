de22cc43deac8424bb75f8d5f76bb6d127931805
Jay Wilson <heyjay@cctplus.dev>
2024-11-08 13:46:43 -0800

    chore!: update iOS version (#40)
    * chore!: update iOS version to 17 to use Swift Data
    
    * chore: update format changes
    
    * docs: update readme

--- README.md
+++ README.md
@@ -6,11 +6,9 @@
 
 - [Node.js](https://nodejs.org) is needed for git hooks
 - Xcode 16 (@heyjaywilson suggests using the [Xcodes app](https://github.com/XcodesOrg/XcodesApp) to manage Xcode versions)
-- [swift-format](https://github.com/apple/swift-format#getting-swift-format)
 
 ### Steps
 
 1. Clone the repo
 2. Navigate into the repo via the command line
 3. Run `npx husky install` to install git hooks
-4. Setup secrets file by duplicating the `Secrets.example.json` and rename it to `Secrets.json`. Use `00000000-0000-0000-0000-000000000000` as the value for `telemetry_deck_key`
--- iHog/iHog/Views/Settings/OSCLog/OSCLogView.swift
+++ iHog/iHog/Views/Settings/OSCLog/OSCLogView.swift
@@ -8,41 +8,42 @@
 import SwiftUI
 
 struct OSCLogView: View {
-    @EnvironmentObject var osc: OSCHelper
-    @State private var logIsPaused = false
-    
-    var body: some View {
-        VStack{
-            Toggle(isOn: $osc.isLogPaused){
-                Text(osc.isLogPaused ? "Resume OSC Log" : "Pause OSC Log")
-            }.onChange(of: logIsPaused){ newValue in
-                    osc.toggleLog(logIsPaused)
-                }
+  @EnvironmentObject var osc: OSCHelper
+  @State private var logIsPaused = false
+
+  var body: some View {
+    VStack {
+      Toggle(isOn: $osc.isLogPaused) {
+        Text(osc.isLogPaused ? "Resume OSC Log" : "Pause OSC Log")
+      }
+      .onChange(of: logIsPaused) {
+        osc.toggleLog(logIsPaused)
+      }
+      .padding(.horizontal)
+      List {
+        ForEach(osc.oscLog.reversed(), id: \.self) { message in
+          HStack {
+            if message["sent"] == "no" {
+              Image(systemName: "arrow.down.square")
                 .padding(.horizontal)
-            List{
-                ForEach(osc.oscLog.reversed(), id: \.self) { message in
-                    HStack{
-                        if message["sent"] == "no" {
-                            Image(systemName: "arrow.down.square")
-                                .padding(.horizontal)
-                                .foregroundColor(.orange)
-                        } else {
-                            Image(systemName: "arrow.up.square")
-                                .padding(.horizontal)
-                                .foregroundColor(.purple)
-                        }
-                        Text(message["message"] ?? "NO MESSAGE")
-                        Spacer()
-                        Text(message["argument"] ?? "NO MESSAGE")
-                    }
-                }
+                .foregroundColor(.orange)
+            } else {
+              Image(systemName: "arrow.up.square")
+                .padding(.horizontal)
+                .foregroundColor(.purple)
             }
+            Text(message["message"] ?? "NO MESSAGE")
+            Spacer()
+            Text(message["argument"] ?? "NO MESSAGE")
+          }
         }
+      }
     }
+  }
 }
 
 struct OSCLogView_Previews: PreviewProvider {
-    static var previews: some View {
-        OSCLogView()
-    }
+  static var previews: some View {
+    OSCLogView()
+  }
 }
