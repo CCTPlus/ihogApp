6be198a4a50b024adf005e54d3be07e91e637c81
heyjaywilson <heyjay@cctplus.dev>
2024-11-10 13:13:38 -0800

    chore: add posthog for feature flagging and secondary analtyics

--- iHog/iHog/Analytics/Analytics.swift
+++ iHog/iHog/Analytics/Analytics.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreData
+import PostHog
 import TelemetryDeck
 
 class Analytics {
@@ -17,6 +18,15 @@ class Analytics {
   init() {
     let config = TelemetryDeck.Config(appID: appID)
     TelemetryDeck.initialize(config: config)
+
+    // Setup posthog for analytics and feature flags
+    // Deciding between telemetry deck and posthog, so why not have both for the time being?
+    let POSTHOG_API_KEY = "phc_VQu7iGYwqrPQW2v2gTH0xalbGruE4iye6zmp2M4Iep2"
+    // usually 'https://us.i.posthog.com' or 'https://eu.i.posthog.com'
+    let POSTHOG_HOST = "https://us.i.posthog.com"
+
+    let postHogConfig = PostHogConfig(apiKey: POSTHOG_API_KEY, host: POSTHOG_HOST)
+    PostHogSDK.shared.setup(postHogConfig)
   }
 
   func updateUserID(with userID: String) {
@@ -33,17 +43,33 @@ class Analytics {
     return numberOfShows ?? 0
   }
 
-  func logEvent(with event: AnalyticEvent, parameters: [AnalyticEventParameter: String] = [:]) {
+  func logEvent(with event: AnalyticEvent, parameters: [AnalyticEventParameter: Any] = [:]) {
     Task {
       var parameters = parameters
-      // Everytime an event occurs, get the numberOfShows
-      parameters[.numberOfShows] = await String(getNumberOfShows())
-      // Convert enum keys to strings
+      parameters[.numberOfShows] = await getNumberOfShows()
+
+      // Convert to [String: String] for TelemetryDeck
       let stringParameters = parameters.reduce(into: [String: String]()) { result, pair in
-        result[pair.key.rawValue] = pair.value
+        let value = pair.value
+        if let rawValueEnum = value as? any RawRepresentable<String> {
+          result[pair.key.rawValue] = rawValueEnum.rawValue
+        } else {
+          result[pair.key.rawValue] = String(describing: value)
+        }
+      }
+
+      // Convert to [String: Any] for PostHog
+      let anyParameters = parameters.reduce(into: [String: Any]()) { result, pair in
+        let value = pair.value
+        if let rawValueEnum = value as? any RawRepresentable<String> {
+          result[pair.key.rawValue] = rawValueEnum.rawValue
+        } else {
+          result[pair.key.rawValue] = value
+        }
       }
 
       TelemetryDeck.signal(event.rawValue, parameters: stringParameters)
+      PostHogSDK.shared.capture(event.rawValue, properties: anyParameters)
       HogLogger.log(category: .analytics)
         .debug("🔦 Logged event: \(event.rawValue) | \(stringParameters)")
     }
--- iHog/iHog/Views/Paywalls/SettingsSectionPaywall.swift
+++ iHog/iHog/Views/Paywalls/SettingsSectionPaywall.swift
@@ -5,58 +5,62 @@
 //  Created by Jay Wilson on 11/8/24.
 //
 
-import SwiftUI
-import StoreKit
 import RevenueCat
+import StoreKit
+import SwiftUI
 
 struct SettingsSectionPaywall: View {
-    @EnvironmentObject var user: UserState
-    
-    private var foundProducts: [Package] {
-        return user.offerings?[RCConstants.Offerings.year.name]?.availablePackages ?? []
-    }
-    
-    var body: some View {
-        VStack(alignment: .center) {
-            HStack {
-                Image(symbol: ._wandandstars)
-                Text("Be a PRO-grammer")
-                Image(symbol: ._wandandstars)
+  @EnvironmentObject var user: UserState
+
+  private var foundProducts: [Package] {
+    return user.offerings?[RCConstants.Offerings.year.name]?.availablePackages ?? []
+  }
+
+  var body: some View {
+    VStack(alignment: .center) {
+      HStack {
+        Image(symbol: ._wandandstars)
+        Text("Be a PRO-grammer")
+        Image(symbol: ._wandandstars)
+      }
+      Text("Unlock unlimited shows and more")
+        .lineLimit(nil)
+      ForEach(foundProducts, id: \.self) { package in
+        Button("Start your \(package.terms(for: package))") {
+          Purchases.shared.purchase(package: package) { (_, customerInfo, error, _) in
+            if let error = error {
+              print(error.localizedDescription)
             }
-            Text("Unlock unlimited shows and more")
-                .lineLimit(nil)
-            ForEach(foundProducts, id: \.self) { package in
-                Button("Start your \(package.terms(for: package))") {
-                    Purchases.shared.purchase(package: package) { (_, customerInfo, error, _) in
-                        if let error = error {
-                            print(error.localizedDescription)
-                        }
-                        if customerInfo?.entitlements[RCConstants.Entitlements.pro.name]?.isActive == true {
-                            user.unlockPro()
-                        }
-                        Analytics.shared.logEvent(with: .subscribeButtonTapped, parameters: [.paywallSource: PaywallSource.settings.rawValue])
-                    }
-                }.buttonStyle(.borderedProminent)
-                Text(setPrice(package))
-                    .font(.footnote)
+            if customerInfo?.entitlements[RCConstants.Entitlements.pro.name]?.isActive == true {
+              user.unlockPro()
             }
-            NavigationLink("Learn more", value: Routes.paywall(.currentPaywall))
-                .font(.footnote)
-                .foregroundColor(.blue)
+            Analytics.shared.logEvent(
+              with: .subscribeButtonTapped,
+              parameters: [.paywallSource: PaywallSource.settings]
+            )
+          }
         }
-        
+        .buttonStyle(.borderedProminent)
+        Text(setPrice(package))
+          .font(.footnote)
+      }
+      NavigationLink("Learn more", value: Routes.paywall(.currentPaywall))
+        .font(.footnote)
+        .foregroundColor(.blue)
     }
+
+  }
 }
 
 #Preview {
-    SettingsSectionPaywall()
+  SettingsSectionPaywall()
 }
 
 extension SettingsSectionPaywall {
-    func setPrice( _ package: Package) -> String {
-        let price = package.localizedPriceString
-        let duration = package.storeProduct.subscriptionPeriod!.durationTitle
-        return "then \(price) per \(duration)"
-    }
-    
+  func setPrice(_ package: Package) -> String {
+    let price = package.localizedPriceString
+    let duration = package.storeProduct.subscriptionPeriod!.durationTitle
+    return "then \(price) per \(duration)"
+  }
+
 }
