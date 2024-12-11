f715ddc9f79526197ed07affd6e804b0774569e4
heyjaywilson <heyjay@cctplus.dev>
2024-12-10 19:20:15 -0800

    chore: use log instead of print

--- iHog/iHog/Helpers/NetworkMonitor.swift
+++ iHog/iHog/Helpers/NetworkMonitor.swift
@@ -8,33 +8,30 @@
 import Network
 
 class NetworkMonitor {
-    static let shared = NetworkMonitor()
+  static let shared = NetworkMonitor()
 
-    let monitor = NWPathMonitor()
-    private var status: NWPath.Status = .requiresConnection
-    var isReachable: Bool { status == .satisfied }
-    var isReachableOnCellular: Bool = true
+  let monitor = NWPathMonitor()
+  private var status: NWPath.Status = .requiresConnection
+  var isReachable: Bool { status == .satisfied }
+  var isReachableOnCellular: Bool = true
 
-    func startMonitoring() {
-        monitor.pathUpdateHandler = { [weak self] path in
-            self?.status = path.status
-            self?.isReachableOnCellular = path.isExpensive
+  func startMonitoring() {
+    monitor.pathUpdateHandler = { [weak self] path in
+      self?.status = path.status
+      self?.isReachableOnCellular = path.isExpensive
 
-            if path.status == .satisfied {
-                print("We're connected!")
-                // post connected notification
-            } else {
-                print("No connection.")
-                // post disconnected notification
-            }
-            print(path.isExpensive)
-        }
-
-        let queue = DispatchQueue(label: "NetworkMonitor")
-        monitor.start(queue: queue)
+      if path.status == .satisfied && path.isExpensive == false {
+        HogLogger.log(category: .default).info("Connected to a network")
+      } else {
+        HogLogger.log(category: .default).info("Not connected to a network")
+      }
     }
 
-    func stopMonitoring() {
-        monitor.cancel()
-    }
+    let queue = DispatchQueue(label: "NetworkMonitor")
+    monitor.start(queue: queue)
+  }
+
+  func stopMonitoring() {
+    monitor.cancel()
+  }
 }
