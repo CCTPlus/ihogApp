a3b989a78896dbf9650cc78d34a9335b2d46e05e
heyjaywilson <heyjay@cctplus.dev>
2025-01-03 08:25:13 -0800

    docs: add analytics values

--- iHog/iHog/Analytics/AnalyticEvent.swift
+++ iHog/iHog/Analytics/AnalyticEvent.swift
@@ -7,15 +7,26 @@
 
 /// Events tracked by analtyics
 enum AnalyticEvent: String {
+  /// Track when the app is launched
   case appLaunched
+  /// Track when the subscribe button is tapped
   case subscribeButtonTapped
+  /// Track when an error occurs
   case error
+  /// Track when a purchase is made
   case purchase
+  /// Track when the connect to console button is tapped
   case connectToConsoleTapped
+  /// Track when the disconnect from console button is tapped
   case disconnectFromConsoleTapped
+  /// Track when the onboarding skip button is tapped to determine if it's useful. More skips means less useful to users
   case onboardingSkipTapped
+  /// Track when the onboarding step is viewed so that we can see how many steps are completed
   case onboardingStepViewed
+  /// Track when the user profile is tapped
   case userProfileTapped
+  /// Track when the user code is loaded
   case userCodeLoaded
+  /// Track when a feature flag is toggled
   case featureFlagToggled
 }
